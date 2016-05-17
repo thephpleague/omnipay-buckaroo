f67c836c36f27c42a18917a1261d62a6a93b0890
Silvano-a <silvano.a@hotmail.com>
2016-05-17 13:45:00 +0200

    Add Buckaroo gateway, Add possibilty to choose credit card provider in creditcard gateway

--- src/Message/CreditCardPurchaseRequest.php
+++ src/Message/CreditCardPurchaseRequest.php
@@ -10,7 +10,7 @@ class CreditCardPurchaseRequest extends AbstractRequest
     public function getData()
     {
         $data = parent::getData();
-        $data['Brq_payment_method'] = 'visa';
+        $data['Brq_payment_method'] = $this->getPaymentMethod();
 
         return $data;
     }
